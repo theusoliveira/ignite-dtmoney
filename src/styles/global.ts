2beafcd2fb445b413cc29e713d94e5024a7a6973
Matheus Oliveira <matheus.oliveira@ivoryit.com.br>
2021-03-17 08:24:31 -0300

    Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --red: #e52e40;
+    --green: #33cc95;
     --blue: #5429cc;
     --blue-light: #6933ff;
     --text-title: #363f5f;
